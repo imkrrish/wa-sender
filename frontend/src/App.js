ebfad716c182e451f39f5320883f99989bad4324
imkrrish <imkrrish227@gmail.com>
2022-12-11 22:41:23 +0530

    added rrd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,10 @@
 import React from "react"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from "react-router-dom"
 import Welcome from "./views/welcome/Welcome"
 import AppBar from "./views/appBar/AppBar"
 import Login from "./views/Login/Login"
@@ -6,15 +12,55 @@ import Home from "./views/home/Home"
 import WriteMessage from "./views/writeMessage/WriteMessage"
 import MessageSent from "./views/messageSent/MessageSent"
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Welcome />} />
+      {/* <AppBar /> */}
+      <Route
+        path="/login"
+        element={
+          <>
+            <AppBar />
+            <Login />
+          </>
+        }
+      />
+      <Route
+        path="/home"
+        element={
+          <>
+            <AppBar />
+            <Home />
+          </>
+        }
+      />
+      <Route
+        path="/writemessage"
+        element={
+          <>
+            <AppBar />
+            <WriteMessage />
+          </>
+        }
+      />
+      <Route
+        path="/messagesent"
+        element={
+          <>
+            <AppBar />
+            <MessageSent />
+          </>
+        }
+      />
+    </>
+  )
+)
+
 function App() {
   return (
     <>
-      {/* <Welcome /> */}
-      <AppBar />
-      {/* <Login /> */}
-      {/* <Home /> */}
-      {/* <WriteMessage /> */}
-      <MessageSent />
+      <RouterProvider router={router} />
     </>
   )
 }
